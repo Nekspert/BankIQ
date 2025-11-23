d2f1a1caad314f66de1e3d5f74e9deed7801a142
Maxim <MaximK38136@yandex.ru>
2025-11-23 22:32:50 +0400

    chore(dependencies): FIX2

--- frontend/src/features/register-form/RegisterForm.tsx
+++ frontend/src/features/register-form/RegisterForm.tsx
@@ -1,5 +1,4 @@
 import { useForm } from 'react-hook-form';
-import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, type FC } from 'react';
